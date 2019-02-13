afaa32506af1be875dac8302eb088945d70621ee
Tomasz Kaleta <tomasz.development@gmail.com>
2019-02-13 15:24:45 +0100

    specific queries

--- src/main/java/com/tomek/JpaSpringBootApplication.java
+++ src/main/java/com/tomek/JpaSpringBootApplication.java
@@ -8,29 +8,65 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class JpaSpringBootApplication {
     public static void main(String[] args) throws InterruptedException {
         ConfigurableApplicationContext ctx = SpringApplication.run(JpaSpringBootApplication.class, args);
-        /*
-        general concept of Spring Data is to define method like that:
-        public List<T> findByName(String name);
-        and spring generates rest of code
-        */
+
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("A4", "Audi", 49000.0));
+        cars.add(new Car("A5", "Audi", 67000.0));
         cars.add(new Car("Auris", "Toyota", 35000.0));
         cars.add(new Car("Insignia", "Opel", 29500.0));
+        cars.add(new Car("A8", "Audi", 28000.0));
+        cars.add(new Car("Corolla", "Toyota", 31000.0));
+        cars.add(new Car("Vectra", "Opel", 29500.0));
+        cars.add(new Car("Astra", "Opel", 29500.0));
 
         CarRepository carRepo = ctx.getBean(CarRepository.class);
-        cars.forEach(carRepo::save); // save all
+        cars.forEach(carRepo::save);
 
-        Car firstCar = carRepo.findById(1L).get(); // get first
-        carRepo.delete(firstCar); // remove it
+        //Find all
+        System.out.println("All Toyota cars:");
+        List<Car> allToyotas = carRepo.findByBrand("Toyota");
+        allToyotas.forEach(System.out::println);
 
-        // get and print all
-        carRepo.findAll().forEach(System.out::println);
+        //Find first
+        Car opel = carRepo.findFirstByBrand("Opel");
+        System.out.println(opel);
+
+        //And / Or
+        System.out.println("Audi for 49000: ");
+        carRepo.findAllByBrandAndPrice("Audi", 49000).forEach(System.out::println);
+        System.out.println("Toyota or Opel:");
+        carRepo.findAllByBrandOrBrand("Toyota", "Opel").forEach(System.out::println);
+
+        //like, notlike, startingwith, endingwith, containing
+        System.out.println("Cars with brand starting with A");
+        carRepo.findAllByBrandLike("A%").forEach(System.out::println);
+        System.out.println("Cars with name ending with 'tra'");
+        carRepo.findAllByNameEndingWith("tra").forEach(System.out::println);
+
+        //lessThan, between
+        System.out.println("Cars cheaper than 30000");
+        carRepo.findAllByPriceLessThan(30000).forEach(System.out::println);
+        System.out.println("Cars with price between 30-40k");
+        carRepo.findAllByPriceBetween(30000, 40000).forEach(System.out::println);
+
+        //ordering
+        System.out.println("All Audi cars, price ascending");
+        carRepo.findAllByBrandOrderByPriceAsc("Audi")
+                .forEach(System.out::println);
+
+        //notNull, in
+        System.out.println("Cars with brand");
+        carRepo.findAllByBrandNotNull().forEach(System.out::println);
+        System.out.println("Car name in {Insignia, Corolla}");
+        List<String> carNames = Stream.of("Insignia", "Corolla").collect(Collectors.toList());
+        carRepo.findAllByNameIn(carNames).forEach(System.out::println);
 
         ctx.close();
     }
--- src/main/java/com/tomek/repository/CarRepository.java
+++ src/main/java/com/tomek/repository/CarRepository.java
@@ -4,11 +4,40 @@ import com.tomek.model.Car;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository // annotation is optional but makes code cleaner
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Repository
 public interface CarRepository extends CrudRepository<Car, Long> {
-    /*
-     interface CarRepository by itself has no methods but in fact extends generic CrudRepository interface
-       CarRepo inherits from CrudRepository and has access to its methods (check docs) even if this class looks empty
-       Spring Data automatically generates implementation of interface
-    */
+
+    //example specific queries
+    // name of the method literally tells what method does
+
+    List<Car> findByBrand(String brand);
+
+    Car findFirstByBrand(String brand);
+
+    Car findFirstByPrice(double price);
+    //Car findTopByPrice(double price);
+
+    List<Car> findFirst3ByBrand(String brand);
+
+    List<Car> findAllByBrandAndPrice(String brand, double price);
+
+    List<Car> findAllByBrandOrBrand(String brand, String brand2);
+
+    List<Car> findAllByBrandLike(String pattern);
+
+    List<Car> findAllByNameEndingWith(String pattern);
+
+    List<Car> findAllByPriceLessThan(double price);
+
+    List<Car> findAllByPriceBetween(double low, double high);
+
+    List<Car> findAllByBrandOrderByPriceAsc(String brand);
+
+    List<Car> findAllByBrandNotNull();
+
+    List<Car> findAllByNameIn(Collection<String> names);
 }
